885cafa5e39c3df49b564c1f07a83bd35ec43181
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-06-03 00:32:38 +0300

    Implement product viewcontroller

---
+++ iOS/arbitrager/arbitrager/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  arbitrager
+//
+//  Created by Matan Lachmish on 02/06/2016.
+//  Copyright © 2016 arbitrager. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class MainViewController: NSViewController {
+
+    @IBOutlet weak var urlTextField: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    override func prepareForSegue(segue: NSStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ProductDetailSegue" {
+            let productVC = segue.destinationController as! ProductViewController
+            productVC.productUrl = urlTextField.stringValue
+            print("Showing product page")
+        }
+    }
+}
+
--- iOS/arbitrager/arbitrager/ProductViewController.swift
+++ iOS/arbitrager/arbitrager/ProductViewController.swift
@@ -1,27 +1,30 @@
 //
-//  ViewController.swift
+//  ProductViewController.swift
 //  arbitrager
 //
-//  Created by Matan Lachmish on 02/06/2016.
+//  Created by Matan Lachmish on 03/06/2016.
 //  Copyright © 2016 arbitrager. All rights reserved.
 //
 
 import Cocoa
+import Foundation
 
-class ViewController: NSViewController {
+class ProductViewController: NSViewController {
+
+    var productUrl : String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: AnyObject? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
